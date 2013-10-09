e130673feed68229d2fb02d167778f41831edeba
Guido Martínez <mtzguido@gmail.com>
2013-10-09 17:14:13 -0300

    run_tests

---
+++ scripts/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for i in tests/bad/*.tig; do
+	if ./tiger "$i" &>/dev/null; then
+		echo
+		echo "Test $i FALLADO!" ;
+		exit 1 ;
+	else
+		echo -n "." ;
+	fi
+done
+for i in tests/good/*.tig; do
+	if ./tiger "$i" &>/dev/null; then
+		echo -n "." ;
+	else
+		echo
+		echo "Test $i FALLADO!" ;
+		exit 1 ;
+	fi
+done
+
+
+echo -e '\nall good'
